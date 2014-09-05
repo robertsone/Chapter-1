641eb5c1f1fc30ad3cbf1a856a1fb500fe0a4bdd
robertsone <rufrrufr@yahoo.com>
2014-09-05 08:14:02 -0400

    V1
    start

---
+++ chapter 1/Address/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Address
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Daniel Burke,\n 2747 Queen Street,\n Easton, PA.");
+        }
+    }
+}
---
+++ chapter 1/Quotations/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Quotations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Its a-me Msario!\n-Mario");
+        }
+    }
+}
